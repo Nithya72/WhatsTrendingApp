fab35884890ae4434670165b24baa36cf7e24552
Jayashree S <jayashrees@Jayashrees-MacBook-Pro.local>
2020-04-24 18:10:42 -0700

    Basic web page

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 from flask import Flask, send_file, render_template
-
 from plot import trending
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html", title='Home Page', noHomePageLink=True)
+    return render_template("index.html", title='Home', noHomePageLink=True)
 
 
 @app.route("/usTrend")
 def usTrend_func():
-    # trending.plot_category()
     html_text = [trending.plot_tags(), trending.plot_publish_hours(), trending.plot_subscribers()]
     return render_template("US.html", title="United States", graph=html_text)
-
 
 @app.route("/canadaTrend")
 def canadaTrend_func():
     return render_template("Canada.html", title="Canada")
+
+
+@app.route("/indiaTrend")
+def indiaTrend_func():
+    return render_template("India.html", title="India")
+
+
+@app.route("/mexicoTrend")
+def mexicoTrend_func():
+    return render_template("Mexico.html", title="Mexico")
+
+
+@app.route("/koreaTrend")
+def koreaTrend_func():
+    return render_template("Korea.html", title="Korea")
+
 
 
 if __name__ == '__main__':
