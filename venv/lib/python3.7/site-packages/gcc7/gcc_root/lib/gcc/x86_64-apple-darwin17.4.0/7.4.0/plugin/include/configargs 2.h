961a72a7a290fdd967f0b9e0ab8e9e73ec3a139f
Linda Nguyen <lindavy28@gmail.com>
2020-04-30 11:48:29 -0700

    modified venv

---
+++ venv/lib/python3.7/site-packages/gcc7/gcc_root/lib/gcc/x86_64-apple-darwin17.4.0/7.4.0/plugin/include/configargs 2.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/travis/gcc_src/configure --prefix /Users/travis/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
