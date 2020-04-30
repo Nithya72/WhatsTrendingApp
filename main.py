f864a32a1a2f50e92c6b86a0738111a22ba86ee9
Linda Nguyen <lindavy28@gmail.com>
2020-04-29 22:43:00 -0700

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_file, render_template
-from plot import trending
+from plot import trending, graphs
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -13,12 +13,14 @@ def home():
 
 @app.route("/usTrend")
 def usTrend_func():
-    html_text = [trending.plot_tags(), trending.plot_publish_hours(), trending.plot_subscribers()]
+    html_text = [trending.plot_tags(), trending.plot_publish_hours(), trending.plot_subscribers(), trending.heatmap()]
     return render_template("US.html", title="United States", graph=html_text)
+
 
 @app.route("/canadaTrend")
 def canadaTrend_func():
-    return render_template("Canada.html", title="Canada")
+    html_text = [graphs.plot_views()]
+    return render_template("Canada.html", title="Canada", graph=html_text)
 
 
 @app.route("/indiaTrend")
@@ -36,8 +38,8 @@ def koreaTrend_func():
     return render_template("Korea.html", title="Korea")
 
 
-
 if __name__ == '__main__':
+    #app.run(host='0.0.0.0', debug=True, port=80)
     app.jinja_env.auto_reload = True
     app.run(debug=True)
     app.config(TEMPLATES_AUTO_RELOAD=True)
--- plot/trending.py
+++ plot/trending.py
@@ -3,21 +3,47 @@ import warnings
 warnings.filterwarnings('always')
 warnings.filterwarnings('ignore')
 
+import pandas as pd
 from pandas import read_csv
 import matplotlib.pyplot as plt, mpld3
 from matplotlib.pyplot import axis
 import seaborn as sb
+import os
+from os import path 
+import sys 
+import time 
+import numpy as np
 
-df = read_csv("USA_trending_videos.csv")
+df = read_csv("dataset/USA_trending_videos.csv")
 
+# def read_file(csv_name: str):
+#     file_path = os.path.join("dataset", csv_name)  # concat path name
+#     if path.exists(file_path):
+#         print("filepath:", file_path)
+#         global df
+#         df = read_csv(file_path)
+#         plot_tags()
+#     else: 
+#         sys.stdout.write("File doesn't exist!")
+#         exit() 
+
+# def create_plot(csv_name: str): 
+#     read_file(csv_name)
+#     time.sleep(1)
+#     #plot_category()
+#     #plot_publish_hours()
+#     plot_tags()
+#     #plot_subscribers()
 
 def plot_category():
+    print("plot_category")
     fig, output = sb.factorplot(data=df, kind='count', size=2, aspect=1, x='title_y')
     output.set_xticklabels(rotation=90)
     return mpld3.fig_to_html(fig)
 
 
 def plot_publish_hours():
+    print("publish_hours")
     fig = plt.figure()
     sb.distplot(df['publish_hour'])
     plt.ylabel("Trending Level")
@@ -36,4 +62,19 @@ def plot_subscribers():
     sums = df.groupby(df["title_y"])["subscriber"].sum()
     axis('equal')
     plt.pie(sums, autopct='%.2f')
+    return mpld3.fig_to_html(fig)
+
+# def heatmap():
+#     fig = plt.figure()
+#     df = pd.DataFrame(np.random.random((10,10)), columns=["a","b","c","d","e","f","g","h","i","j"])
+#     sb.heatmap(df, cmap="YlGnBu")
+#     return mpld3.fig_to_html(fig)
+
+
+def heatmap():
+    fig = plt.figure()
+    corrolation_list = ['views', 'likes', 'dislikes', 'comment_count']
+    hm_data = df[corrolation_list].corr()
+    fig.savefig(sb.pairplot(df[['views', 'likes']], kind='reg'))
+    fig = plt.show()
     return mpld3.fig_to_html(fig)
