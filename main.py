f7df2f5709572df60d927255f32f5a86637da79b
Jayashree S <jayashrees@Jayashrees-MacBook-Pro.local>
2020-05-01 19:34:19 -0700

    Added new ui changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, request
 from plot import usaTrending
-from plot import indTrending
+from plot import showTrending
+from plot import sentiment
 from flask_bootstrap import Bootstrap
-from flask import Flask, request
+from plot import dataFormatter
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -13,39 +15,76 @@ def home():
     return render_template("index.html", title='Home', noHomePageLink=True)
 
 
+@app.route("/videoInput")
+def getIdForSentimentAnalysis_func():
+    return render_template("videoInput.html", title='Analyze your data set')
+
+
+@app.route("/sentiment", methods=["POST"])
+def sentimentAnalysis():
+    videoId = request.form['videoId']
+    html_text = [sentiment.get_videoid(videoId)]
+    return render_template("videoInput.html", title="Sentimental Analysis", graph=html_text)
+
+
+@app.route("/countries")
+def countries_func():
+    return render_template("countries.html", title='Countries')
+
+
 @app.route("/usTrend")
 def usTrend_func():
-    html_text = [[usaTrending.plot_category(), "cat"],
-                 [usaTrending.plot_publish_hours(), "hrs"],
-                 [usaTrending.plot_tags(), "tag"],
-                 [usaTrending.plot_tags_word_cloud(), "cloud"]]
+    html_text = [usaTrending.plot_category(),
+                 usaTrending.plot_publish_hours(),
+                 usaTrending.plot_tags(),
+                 usaTrending.plot_tags_word_cloud(), "cloud"]
     return render_template("US.html", title="United States", graph=html_text)
 
 @app.route("/canadaTrend")
 def canadaTrend_func():
     return render_template("Canada.html", title="Canada")
 
-@app.route('/video_id', methods=['POST'])
-def video_id():
-    videoID = request.form['videoId']
-    html_text = [indTrending.get_videoid(videoID)]
-    return render_template("India.html", title="United States", graph=html_text)
-
 
 @app.route("/indiaTrend")
 def indiaTrend_func():
-    html_text = [indTrending.plot_senti()]
-    return render_template("India.html", title="India", graph=html_text)
+    dataFormatter.processData("./dataset/INvideos.csv", './dataset/IN_category_id.json', 'IN')
+    df = pd.read_csv("./transformed/IN_trending_videos.csv")
+    html_text = [showTrending.plot_category(df), showTrending.plot_publish_hours(df),
+                 showTrending.plot_tags_word_cloud(df)]
+    return render_template("India.html", title="India")
 
 
 @app.route("/mexicoTrend")
 def mexicoTrend_func():
+    dataFormatter.processData("./dataset/MXvideos.csv", './dataset/MX_category_id.json', 'FR')
+    df = pd.read_csv("./transformed/KR_trending_videos.csv")
+    html_text = [showTrending.plot_category(df), showTrending.plot_publish_hours(df),
+                 showTrending.plot_tags_word_cloud(df)]
     return render_template("Mexico.html", title="Mexico")
 
 
 @app.route("/koreaTrend")
 def koreaTrend_func():
+    dataFormatter.processData("./dataset/KRvideos.csv", './dataset/KR_category_id.json', 'KR')
+    df = pd.read_csv("./transformed/KR_trending_videos.csv")
+    html_text = [showTrending.plot_category(df), showTrending.plot_publish_hours(df),
+                 showTrending.plot_tags_word_cloud(df)]
     return render_template("Korea.html", title="Korea")
+
+
+@app.route("/franceTrend")
+def franceTrend_func():
+    dataFormatter.processData("./dataset/FRvideos.csv", './dataset/FR_category_id.json', 'FR')
+    df = pd.read_csv("./transformed/FR_trending_videos.csv")
+    html_text = [showTrending.plot_category(df), showTrending.plot_publish_hours(df), showTrending.plot_tags_word_cloud(df)]
+    return render_template("Mexico.html", title="Mexico", graph=html_text)
+
+@app.route("/gbTrend")
+def gbTrend_func():
+    dataFormatter.processData("./dataset/GBvideos.csv", './dataset/GB_category_id.json', 'GB')
+    df = pd.read_csv("./transformed/GB_trending_videos.csv")
+    html_text = [showTrending.plot_category(df), showTrending.plot_tags_word_cloud(df)]
+    return render_template("gb.html", title="Great Britain", graph=html_text)
 
 
 if __name__ == '__main__':
