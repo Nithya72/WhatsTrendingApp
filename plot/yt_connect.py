bdf357f574f3b6c05d8bc849ab18a2631df9f428
Nithya72 <60639346+Nithya72@users.noreply.github.com>
2020-05-03 12:46:44 -0700

    updated the senti analysis tool

--- plot/yt_connect.py
+++ plot/yt_connect.py
@@ -1,13 +1,15 @@
 # Reference from - https://medium.com/@yuhuiluo/automating-youtube-comment-sentiment-analysis-b4e2969eb62f
 
 import httplib2
-import os
+import re
 
-# nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+from nltk.tokenize import word_tokenize
+
+from nltk.corpus import stopwords
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from apiclient.discovery import build_from_document
-# from googleapiclient.discovery import build_from_document
-from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
@@ -18,17 +20,8 @@ YOUTUBE_READ_WRITE_SSL_SCOPE = "https://www.googleapis.com/auth/youtube.force-ss
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-MISSING_CLIENT_SECRETS_MESSAGE = """
-WARNING: Please configure OAuth 2.0
-To make this sample run you will need to populate the client_secrets.json file
-found at:
-   %s
-with information from the APIs Console
-https://console.developers.google.com
-For more information about the client_secrets.json file format, please visit:
-https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))
+MISSING_CLIENT_SECRETS_MESSAGE = """WARNING: Please configure OAuth 2.0
+To make this sample run you will need to populate the client_secrets.json file"""
 
 def get_authenticated_service(args):
     flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE, message=MISSING_CLIENT_SECRETS_MESSAGE)
@@ -45,9 +38,6 @@ def get_authenticated_service(args):
 
 
 def get_comment_threads(youtube, video_id, comments=[], token=""):
-
-    # print("Comments length - ", len(comments))
-
     results = youtube.commentThreads().list(
         part="snippet",
         pageToken=token,
@@ -66,21 +56,39 @@ def get_comment_threads(youtube, video_id, comments=[], token=""):
     else:
         return comments
 
-def generate(video_id):
+def process_comments(video_comment_threads):
 
+    comments = []
+    for comment in video_comment_threads:
+        stop_words = list(set(stopwords.words('english')))
+        allowed_word_types = ["JJ"]
+
+        cleaned = re.sub(r'[^(a-zA-Z)\s]', '', str(comment))
+
+        tokenized = word_tokenize(cleaned)
+        print(tokenized)
+        stopped = [w for w in tokenized if not w in stop_words]
+
+        pos = nltk.pos_tag(stopped)
+        # print(pos)
+        for words in pos:
+            if words[1] in allowed_word_types:
+                comments.append(words[0].lower())
+    return comments
+
+
+def generate(video_id):
     youtube = get_authenticated_service(video_id)
     video_comment_threads = get_comment_threads(youtube, video_id, [])
+    # comments = process_comments(video_comment_threads)
     sia = SentimentIntensityAnalyzer()
 
     pol_comments = []
-    for comments in video_comment_threads:
-        score = sia.polarity_scores(comments)['compound']
-        pol_comments.append((comments, score))
+    for c in video_comment_threads:
+        score = sia.polarity_scores(c)['compound']
+        pol_comments.append((c, score))
+
+    # for comment in pol_comments:
+    #     print(comment)
 
     return pol_comments
-
-
-# def main():
-#     generate("0mlNzVSJrT0")
-
-# main()
