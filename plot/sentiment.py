13d646463c273e26858185bec2ccd5a05623c3a2
Nithya72 <60639346+Nithya72@users.noreply.github.com>
2020-05-03 12:47:35 -0700

    Removed commented code block

--- plot/sentiment.py
+++ plot/sentiment.py
@@ -2,108 +2,9 @@ import matplotlib.pyplot as plt, mpld3
 from matplotlib.pyplot import pie, axis, show
 import io
 
-def plot_senti():
-
-    # video_id = "hPmBSTgBmes" - positive
-    video_id1 = "DQDj074iEuM" #mocking negtaive
-
-#     from plot import yt_connect
-#     pol_comments = yt_connect.generate(video_id1)
-#
-#     print("plot comments", len(pol_comments))
-#     neutral = 0;
-#     positive = 0;
-#     negative = 0;
-#
-#     for com in pol_comments:
-#         # print(com[1])
-#         if (com[1] >= 0.05):
-#             positive = positive + 1;
-#         elif (com[1] <= -0.05):
-#             negative = negative + 1;
-#         else:
-#             neutral = neutral + 1;
-#
-#     names = ["Positive", "Negtaive", "Neutral"]
-#     values = [positive, negative, neutral]
-#
-#     fig, ax = plt.subplots()
-#     explode = (0, 0.01, 0)
-#     ax.pie(values, labels=names, autopct='%1.0f%%', explode=explode,
-#            shadow=False, startangle=0, labeldistance=1.2)
-#     ax.axis('equal')
-#     return mpld3.fig_to_html(fig)
-
-
-
-
-#
-# def plot_senti1():
-#     video_id1 = "DQDj074iEuM"
-#
-#     pol_comments = []
-#     from plot import main2
-#     pol_comments = main2.generate(video_id1)
-#
-#     print("plot 1 comments", len(pol_comments))
-#     neutral = 0;
-#     positive = 0;
-#     negative = 0;
-#
-#     for com in pol_comments:
-#         # print(com[1])
-#         if (com[1] >= 0.05):
-#             positive = positive + 1;
-#         elif (com[1] <= -0.05):
-#             negative = negative + 1;
-#         else:
-#             neutral = neutral + 1;
-#
-#     # if(negative > positive):
-#     #     negative = negative+neutral
-#     # elif(positive < negative):
-#     #     positive = positive+neutral
-#
-#     names = ["positive", "negtaive", "neutral"]
-#     values = [positive, negative, neutral]
-#
-#     fig = plt.figure(figsize=(3,3))
-#     plt.bar(names, values)
-#     return mpld3.fig_to_html(fig)
-#
-#
-# def plot_senti2():
-#     video_id1 = "DQDj074iEuM"
-#
-#     pol_comments = []
-#     from plot import main2
-#     pol_comments = main2.generate(video_id1)
-#
-#     print("plot 2 comments", len(pol_comments))
-#     neutral = 0;
-#     positive = 0;
-#     negative = 0;
-#
-#     for com in pol_comments:
-#         # print(com[1])
-#         if (com[1] >= 0.05):
-#             positive = positive + 1;
-#         elif (com[1] <= -0.05):
-#             negative = negative + 1;
-#         else:
-#             neutral = neutral + 1;
-#
-#     names = ["positive", "negtaive", "neutral"]
-#     values = [positive, negative, neutral]
-#
-#     fig = plt.figure(figsize=(3, 3))
-#     plt.bar(names, values)
-#     return mpld3.fig_to_html(fig)
-
 def get_videoid(video_id):
-    print("Done!")
-    print("video_id", video_id)
 
+    print("video_id", video_id)
     from plot import yt_connect
     pol_comments = yt_connect.generate(video_id)
 
@@ -114,19 +15,22 @@ def get_videoid(video_id):
 
     for com in pol_comments:
         # print(com[1])
-        if (com[1] >= 0.05):
+        if (com[1] >= 0.01):
             positive = positive + 1;
-        elif (com[1] <= -0.05):
+        elif (com[1] <= -0.01):
             negative = negative + 1;
         else:
             neutral = neutral + 1;
 
-    names = ["Positive", "Negative", "Neutral"]
-    values = [positive, negative, neutral]
+    names = ["Neutral", "Negative", "Positive"]
+    values = [neutral, negative, positive]
+
+    # names = ["Positive", "Negative"]
+    # values = [positive, negative]
 
     fig, ax = plt.subplots()
-    explode = (0, 0.01, 0)
+    explode = (0, 0, 0.01)
     ax.pie(values, labels=names, autopct='%1.0f%%', explode=explode,
-           shadow=False, startangle=0, labeldistance=1.2)
+           shadow=False, startangle=0, labeldistance=1.05)
     ax.axis('equal')
     return mpld3.fig_to_html(fig)
